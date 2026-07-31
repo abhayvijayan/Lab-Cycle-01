9fa0a12e70f8e5ee896ab166400076dc783c8faa
abhayminer01 <abhayvapvl@gmail.com>
2026-07-31 15:12:24 +0530

    program 1

--- program-1/main.py
+++ program-1/main.py
@@ -1,1 +1,25 @@
-print('demo')
+# COLLECTING INPUT AND TYPE CASTING
+a = int(input('Enter First Number : '))
+b = int(input('Enter Second Number : '))
+
+# ARITHMETIC OPERATIONS
+# ADDITION
+print('Addition : ', a + b)
+
+# SUBTRACTION
+print('Subtraction : ', a - b)
+
+# DIVISION
+print('Division : ', a / b)
+
+# MULTIPLICATION
+print('Multiplication : ', a * b)
+
+# MODULUS
+print('Modulus : ', a % b)
+
+# FLOOR DIVISION
+print('Floor Division : ', a // b)
+
+# EXPONENTIATION
+print('Exponentiation : ', a ** b)
